6f5892ed432c7319f4daafe82c280f95bab8b9a2
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-13 20:52:27 -0300

    chore: Update SwaggerConfig for Measure Converter API documentation

--- src/main/java/com/api/measureconverter/config/SwaggerConfig.java
+++ src/main/java/com/api/measureconverter/config/SwaggerConfig.java
@@ -11,26 +11,26 @@ import io.swagger.v3.oas.models.info.License;
 @Configuration
 public class SwaggerConfig {
 
-    private static final String[] END_POINTS = {
-            "/api/convert/**",
-    };
+        private static final String[] END_POINTS = {
+                        "/api/convert/**", "/api/user/**"
+        };
 
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .pathsToMatch(END_POINTS)
-                .group("public-api")
-                .build();
-    }
+        @Bean
+        public GroupedOpenApi publicApi() {
+                return GroupedOpenApi.builder()
+                                .pathsToMatch(END_POINTS)
+                                .group("public-api")
+                                .build();
+        }
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("All in One Converter API")
-                        .version("1.0")
-                        .description("An all-in-one API for converting units")
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title("All in One Converter API")
+                                                .version("1.0")
+                                                .description("An all-in-one API for converting units")
+                                                .termsOfService("http://swagger.io/terms/")
+                                                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+        }
 }
