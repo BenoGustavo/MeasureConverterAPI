be3953a7eb4c2fccac5356ec42f1f2c267a5860a
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-13 16:23:43 -0300

    feat: Add Response and ResponseError classes for API response handling

---
+++ src/main/java/com/api/measureconverter/utils/reponse/Response.java
@@ -0,0 +1,49 @@
+package com.api.measureconverter.utils.reponse;
+
+import lombok.Data;
+
+@Data
+public class Response<T> {
+    private int status;
+    private String result;
+    private ResponseError error;
+    private T data;
+
+    private Response(Builder<T> builder) {
+        this.status = builder.status;
+        this.result = builder.result;
+        this.error = builder.error;
+        this.data = builder.data;
+    }
+
+    public static class Builder<T> {
+        private int status;
+        private String result;
+        private ResponseError error;
+        private T data;
+
+        public Builder<T> status(int status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder<T> result(String result) {
+            this.result = result;
+            return this;
+        }
+
+        public Builder<T> error(ResponseError error) {
+            this.error = error;
+            return this;
+        }
+
+        public Builder<T> data(T data) {
+            this.data = data;
+            return this;
+        }
+
+        public Response<T> build() {
+            return new Response<>(this);
+        }
+    }
+}
