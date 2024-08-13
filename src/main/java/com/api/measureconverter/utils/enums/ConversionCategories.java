d44daaad5cc81b5af808c9f997a5444b3fc21157
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-13 15:36:20 -0300

    feat: Add ConversionCategories enum for measure conversion types

---
+++ src/main/java/com/api/measureconverter/utils/enums/ConversionCategories.java
@@ -0,0 +1,19 @@
+package com.api.measureconverter.utils.enums;
+
+public enum ConversionCategories {
+    LENGTH,
+    WEIGHT,
+    DATA_SIZE,
+    AREA,
+    VOLUME,
+    TEMPERATURE,
+    TIME,
+    SPEED,
+    PRESSURE,
+    ENERGY,
+    POWER,
+    FORCE,
+    ANGLE,
+    FREQUENCY,
+    ELECTRIC_CURRENT
+}
