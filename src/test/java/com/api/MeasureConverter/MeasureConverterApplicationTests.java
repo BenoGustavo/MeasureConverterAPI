c725b36c7a98ad85fd521db0c27866b1e2d54fc5
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-12 22:49:21 -0300

    feat: initial commit

---
+++ src/test/java/com/api/MeasureConverter/MeasureConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.MeasureConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasureConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
