241f99ec6c6c123b5508520cbd50460358a04857
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-13 15:46:52 -0300

    chore: Refactor SwaggerConfig for Measure Converter API documentation

--- src/main/java/com/api/measureconverter/config/SwaggerConfig.java
+++ src/main/java/com/api/measureconverter/config/SwaggerConfig.java
@@ -10,23 +10,26 @@ import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class SwaggerConfig {
+
     private static final String[] END_POINTS = {
             "/api/convert/**",
     };
 
     @Bean
     public GroupedOpenApi publicApi() {
-        GroupedOpenApi.Builder groupedOpenApi = GroupedOpenApi.builder();
-        return groupedOpenApi.pathsToMatch(END_POINTS).group("public-api").build();
+        return GroupedOpenApi.builder()
+                .pathsToMatch(END_POINTS)
+                .group("public-api")
+                .build();
     }
 
     @Bean
-    OpenAPI customOpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("All in one converter API")
+                        .title("All in One Converter API")
                         .version("1.0")
-                        .description("A all in one API for converting units")
+                        .description("An all-in-one API for converting units")
                         .termsOfService("http://swagger.io/terms/")
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
