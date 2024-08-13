9ce10648f785c3d8891a57cf378e657ab2c66be4
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-13 20:53:56 -0300

    feat: Add UserRepository interface for user data retrieval and manipulation

---
+++ src/main/java/com/api/measureconverter/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.api.measureconverter.repositories;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.api.measureconverter.model.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    @Query("SELECT u FROM UserEntity u WHERE u.deleteAt IS NULL")
+    List<UserEntity> findAllActive();
+
+    @Query("SELECT u FROM UserEntity u WHERE u.id = :id AND u.deleteAt IS NULL")
+    UserEntity findByIdActive(UUID id);
+}
