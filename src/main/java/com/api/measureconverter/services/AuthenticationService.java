bb578c058bef8cfa892385aa6c40a95d9a3263bc
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-15 11:48:22 -0300

    refactor: Add AuthenticationService for user authentication and registration

---
+++ src/main/java/com/api/measureconverter/services/AuthenticationService.java
@@ -0,0 +1,56 @@
+package com.api.measureconverter.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.api.measureconverter.error.custom.Unauthorized401Exception;
+import com.api.measureconverter.model.UserEntity;
+import com.api.measureconverter.repositories.UserRepository;
+import com.api.measureconverter.utils.dto.LoginDto;
+import com.api.measureconverter.utils.dto.RegisterDto;
+import com.api.measureconverter.utils.enums.Roles;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@Service
+public class AuthenticationService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public UserEntity signup(@RequestBody RegisterDto registerDto) {
+        UserEntity userEntity = registerDto.toEntity();
+        userEntity.setRole(Roles.USER);
+        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+
+        return userRepository.save(userEntity);
+    }
+
+    public UserEntity authenticate(LoginDto loginDto) throws AuthenticationException, UsernameNotFoundException {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginDto.getEmail(),
+                            loginDto.getPassword()));
+        } catch (AuthenticationException e) {
+            throw new Unauthorized401Exception("Invalid email or password");
+        }
+
+        UserEntity user = userRepository.findByEmail(loginDto.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        if (user.getDeleteAt() != null) {
+            throw new Unauthorized401Exception("Deleted user");
+        }
+
+        return user;
+    }
+}
