74489cda7d324422b050f24327d25f13471de096
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-15 14:43:47 -0300

    refactor: Add UserAlreadyDeletedException for handling already deleted users

--- src/main/java/com/api/measureconverter/services/UserService.java
+++ src/main/java/com/api/measureconverter/services/UserService.java
@@ -5,9 +5,14 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.api.measureconverter.error.custom.NotFound404Exception;
+import com.api.measureconverter.error.custom.Unauthorized401Exception;
+import com.api.measureconverter.error.custom.UserAlreadyDeletedException;
 import com.api.measureconverter.model.UserEntity;
 import com.api.measureconverter.repositories.UserRepository;
 import com.api.measureconverter.utils.dto.RegisterDto;
@@ -43,12 +48,27 @@ public class UserService {
     }
 
     public UserDto delete(UUID id) {
+        if (!isUserOwner(id)) {
+            throw new Unauthorized401Exception("You are not the owner of this user");
+        }
+
         UserEntity user = userRepository.findById(id).orElseThrow();
         if (user.getDeleteAt() != null) {
-            throw new RuntimeException("User already deleted");
+            throw new UserAlreadyDeletedException("User already deleted");
         }
 
         user.setDeleteAt(LocalDateTime.now());
         return userRepository.save(user).toDto();
     }
+
+    private boolean isUserOwner(UUID id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        String idEmail = userRepository.findById(id)
+                .orElseThrow(() -> new NotFound404Exception("User " + email + " not found"))
+                .getEmail();
+
+        return email.equals(idEmail);
+    }
+
 }
