b63345e732f73d5401abf402de22e2d7348fb01f
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-15 14:23:40 -0300

    refactor: Update SecurityConfig to use JWT authentication and authorization

--- src/main/java/com/api/measureconverter/config/SecurityConfig.java
+++ src/main/java/com/api/measureconverter/config/SecurityConfig.java
@@ -16,46 +16,46 @@ import com.api.measureconverter.filter.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
-    @Autowired
-    AuthenticationProvider authenticationProvider;
-    @Autowired
-    JwtAuthenticationFilter jwtAuthenticationFilter;
+        @Autowired
+        AuthenticationProvider authenticationProvider;
+        @Autowired
+        JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    private final String[] WHITE_LIST = {
-            "/api/auth/**", "/", "/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**",
-            "/swagger-ui/**",
-    };
-
-    private final static String[] ONLY_ADMIN_ALLOWED_ROUTES = {
-            "/api/user/admin/**",
-    };
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers(WHITE_LIST).permitAll()
-                        .requestMatchers(ONLY_ADMIN_ALLOWED_ROUTES).hasRole("ADMIN")
-                        .anyRequest().authenticated())
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(sessionManagement -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
+        private final String[] WHITE_LIST = {
+                        "/api/auth/**", "/", "/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**",
+                        "/swagger-ui/**",
         };
-    }
+
+        private final static String[] ONLY_ADMIN_ALLOWED_ROUTES = {
+                        "/api/user/admin/**",
+        };
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                                                .requestMatchers(WHITE_LIST).permitAll()
+                                                .requestMatchers(ONLY_ADMIN_ALLOWED_ROUTES).hasRole("ADMIN")
+                                                .anyRequest().authenticated())
+                                .csrf(csrf -> csrf.disable())
+                                .sessionManagement(sessionManagement -> sessionManagement
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authenticationProvider(authenticationProvider)
+                                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+                return http.build();
+        }
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+                return new WebMvcConfigurer() {
+                        @Override
+                        public void addCorsMappings(@NonNull CorsRegistry registry) {
+                                registry.addMapping("/**")
+                                                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                                                .allowedOrigins("*")
+                                                .allowedHeaders("*");
+                        }
+                };
+        }
 }
