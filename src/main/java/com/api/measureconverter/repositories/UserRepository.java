af7dec6789a195f88c76804fafc0234b954d2049
Gustavo <gustavoleandrogorges@gmail.com>
2024-08-16 11:50:44 -0300

    chore: Add updateLastLogin method to UserRepository

--- src/main/java/com/api/measureconverter/repositories/UserRepository.java
+++ src/main/java/com/api/measureconverter/repositories/UserRepository.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.api.measureconverter.model.UserEntity;
 
@@ -19,4 +22,9 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmail(String email);
 
     boolean existsByEmailAndUsername(String email, String username);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE UserEntity u SET u.lastLogin = CURRENT_TIMESTAMP WHERE u.id = :id")
+    void updateLastLogin(@Param("id") UUID id);
 }
